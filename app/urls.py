bc041fb07da8bfdc8627a82f328d77dd43554b2f
Israel COULIBALY <israelcoulibaly@MacBook-Pro-de-Israel.local>
2021-10-04 01:33:19 +0000

    first commit

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+
+    path('', views.index, name='home'),
+    path('home', views.index, name='home'),
+    path('contact', views.contact, name='contact'),
+    # path('list', views.list, name='list'),
+    # path('detail/<int:id>', views.detail, name='detail'),
+]
